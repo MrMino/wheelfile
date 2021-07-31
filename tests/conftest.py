69bf3e030916664e02aba50b6418c76be3a1d521
Blazej Michalik <blazej.michalik@nokia.com>
2021-07-31 09:37:01 +0200

    Move wf fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,16 @@ import pytest
 
 from io import BytesIO
 
+from wheelfile import WheelFile
+
 
 @pytest.fixture
 def buf():
     return BytesIO()
+
+
+@pytest.fixture
+def wf(buf):
+    wf = WheelFile(buf, 'w', distname='_', version='0')
+    yield wf
+    wf.close()
--- tests/test_wheelfile.py
+++ tests/test_wheelfile.py
@@ -31,13 +31,6 @@ def test_BadWheelFileError_is_ValueError():
 
 
 @pytest.fixture
-def wf(buf):
-    wf = WheelFile(buf, 'w', distname='_', version='0')
-    yield wf
-    wf.close()
-
-
-@pytest.fixture
 def tmp_file(tmp_path):
     fp = tmp_path / 'wheel-0-py3-none-any.whl'
     fp.touch()
