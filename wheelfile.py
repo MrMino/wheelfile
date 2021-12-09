86b61a315537f661e5ebfcb56a53671f35b3f464
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2021-12-09 14:50:50 +0000

    ensure ZipInfo is propagated for data and distinfo directories, unmark the tests as xfail

--- wheelfile.py
+++ wheelfile.py
@@ -1441,9 +1441,7 @@ class WheelFile:
                 new_arcname = new_wf.distinfo_dirname + '/' + arcname_tail
 
                 # create a new ZipInfo
-                new_zinfo = zipfile.ZipInfo(filename=new_arcname,
-                                            date_time=zinfo.date_time)
-                new_zinfo.external_attr = zinfo.external_attr
+                new_zinfo = self._clone_and_rename_zipinfo(zinfo, new_arcname)
 
                 new_wf.writestr(new_zinfo, wf.zipfile.read(zinfo))
                 continue
@@ -1451,9 +1449,7 @@ class WheelFile:
                 new_arcname = new_wf.data_dirname + '/' + arcname_tail
 
                 # create a new ZipInfo
-                new_zinfo = zipfile.ZipInfo(filename=new_arcname,
-                                            date_time=zinfo.date_time)
-                new_zinfo.external_attr = zinfo.external_attr
+                new_zinfo = self._clone_and_rename_zipinfo(zinfo, new_arcname)
 
                 new_wf.writestr(new_zinfo, wf.zipfile.read(zinfo))
                 continue
@@ -1461,6 +1457,19 @@ class WheelFile:
             new_wf.writestr(zinfo, wf.zipfile.read(zinfo))
 
         return new_wf
+
+    @staticmethod
+    def _clone_and_rename_zipinfo(self, zinfo: zipfile.ZipInfo, new_name: str):
+        # attributes to preserved
+        PRESERVED_ZIPINFO_ATTRS = ['date_time', 'compress_type', 'comment',
+                                   'extra', 'create_system', 'create_version',
+                                   'extract_version', 'flag_bits', 'volume',
+                                   'internal_attr', 'external_attr']
+        new_zinfo = zipfile.ZipInfo(filename=new_name)
+        for attr in PRESERVED_ZIPINFO_ATTRS:
+            setattr(new_zinfo, attr, getattr(zinfo, attr))
+
+        return new_zinfo
 
     @staticmethod
     def _is_unnamed_or_directory(target: Union[Path, BinaryIO]) -> bool:
@@ -1983,7 +1992,6 @@ class WheelFile:
         path = os.path.join(arcname, directory[len(prefix):], stem)
         return path
 
-    # TODO: Make sure fields of given ZipInfo objects are propagated
     def writestr(self,
                  zinfo_or_arcname: Union[zipfile.ZipInfo, str],
                  data: Union[bytes, str],
@@ -2107,7 +2115,6 @@ class WheelFile:
 
     # TODO: drive letter should be stripped from the arcname the same way
     # ZipInfo.from_file does it
-    # TODO: Make sure fields of given ZipInfo objects are propagated
     def writestr_data(self, section: str,
                       zinfo_or_arcname: Union[zipfile.ZipInfo, str],
                       data: Union[bytes, str],
@@ -2159,6 +2166,10 @@ class WheelFile:
 
         arcname = self._distinfo_path(section + '/' + arcname.lstrip('/'),
                                       kind='data')
+
+        # clone the rest of the attributes from provided zinfo
+        if isinstance(zinfo_or_arcname, zipfile.ZipInfo):
+            arcname = self._clone_and_rename_zipinfo(zinfo_or_arcname, arcname)
 
         self.writestr(arcname, data, compress_type, compresslevel)
 
@@ -2248,7 +2259,6 @@ class WheelFile:
         self.write(filename, arcname, compress_type, compresslevel,
                    recursive=recursive, skipdir=skipdir)
 
-    # TODO: Make sure fields of given ZipInfo objects are propagated
     def writestr_distinfo(self, zinfo_or_arcname: Union[zipfile.ZipInfo, str],
                           data: Union[bytes, str],
                           compress_type: Optional[int] = None,
@@ -2306,6 +2316,10 @@ class WheelFile:
             )
 
         arcname = self._distinfo_path(arcname.lstrip('/'))
+        # clone the rest of the attributes from provided zinfo
+        if isinstance(zinfo_or_arcname, zipfile.ZipInfo):
+            arcname = self._clone_and_rename_zipinfo(zinfo_or_arcname, arcname)
+
         self.writestr(arcname, data, compress_type, compresslevel)
 
     @staticmethod
