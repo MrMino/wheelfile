0a9ae3824d47cf80c82a40e31f762f16c1b832bd
Jesse P. Johnson <jpj6652@gmail.com>
2022-09-28 20:19:50 -0400

    build: implement pep621 with example build file

--- __build__.py
+++ __build__.py
@@ -1,48 +1,70 @@
-from wheelfile import WheelFile, __version__
+import tarfile
 from pathlib import Path
-from typing import Dict, Any
+from typing import Any, Dict, Optional
 
-# For WheelFile.__init__
-# Platform, abi, and language tags stay as defaults: "py3-none-any"
-spec: Dict[str, Any] = {
-    'distname': 'wheelfile',
-    'version': __version__
-}
+import toml
+from wheelfile import WheelFile
 
-# Fetch requirements into a list of strings
-requirements = Path('./requirements.txt').read_text().splitlines()
 
-# Open a new wheel file
-with WheelFile(mode='w', **spec) as wf:
-    # Add requirements to the metadata
-    wf.metadata.requires_dists = requirements
-    # We target Python 3.6+ only
-    wf.metadata.requires_python = '>=3.6'
+def get_config() -> Dict[str, Any]:
+    """Read pyproject.toml"""
+    project_config = toml.load('pyproject.toml')
+    config = project_config['project']
+    return config
 
-    # Make sure PyPI page renders nicely
-    wf.metadata.summary = "API for inspecting and creating .whl files"
-    wf.metadata.description = Path('./README.md').read_text()
-    wf.metadata.description_content_type = 'text/markdown'
 
-    # Keywords and trove classifiers, for better searchability
-    wf.metadata.keywords = ['wheel', 'packaging', 'pip', 'build', 'distutils']
-    wf.metadata.classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: System :: Archiving :: Packaging',
-        'Topic :: System :: Software Distribution',
-        'Topic :: Utilities',
-    ]
+def build_sdist(
+    sdist_directory: str, config_settings: Dict[str, Any] = None
+) -> str:
+    config = get_config()
+    name = config['name']
+    version = config['version']
+    distname = f'{name}-{version}'
+    filename = f'{distname}.tar.gz'
+    filepath = Path(sdist_directory) / filename
 
-    # Let the world know who is responsible for this
-    wf.metadata.author = "Błażej Michalik"
-    wf.metadata.home_page = 'https://github.com/MrMino/wheelfile'
+    with tarfile.open(filepath, 'w:gz', format=tarfile.PAX_FORMAT) as sdist:
+        sdist.add('./', arcname=distname, filter=None)
+        return filename
 
-    # Add the code - it will install inside site-packages/wheelfile.py
-    wf.write('./wheelfile.py')
+
+def build_wheel(
+    wheel_directory: str,
+    config_settings: Optional[Dict[str, Any]] = None,
+    metadata_directory: Optional[str] = None
+) -> str:
+    config = get_config()
+
+    # For WheelFile.__init__
+    # Platform, abi, and language tags stay as defaults: "py3-none-any"
+    spec: Dict[str, Any] = {
+        'distname': config['name'],
+        'version': config['version']
+    }
+
+    # Open a new wheel file
+    with WheelFile(mode='w', **spec) as wf:
+        # Add requirements to the metadata
+        wf.metadata.requires_dists = config['dependencies']
+        # We target Python 3.6+ only
+        wf.metadata.requires_python = config['requires-python']
+
+        # Make sure PyPI page renders nicely
+        wf.metadata.summary = config['description']
+        wf.metadata.description = Path(config['readme']).read_text()
+        wf.metadata.description_content_type = 'text/markdown'
+
+        # Keywords and trove classifiers, for better searchability
+        wf.metadata.keywords = config['keywords']
+        wf.metadata.classifiers = config['classifiers']
+
+        # Let the world know who is responsible for this
+        wf.metadata.author = config['authors'][0]['name']
+        wf.metadata.home_page = config['urls']['repository']
+
+        # Add the code - it will install inside site-packages/wheelfile.py
+        wf.write('./wheelfile.py')
+        return wf.filename
 
 # Done!
 # 🧀
