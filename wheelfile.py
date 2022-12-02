40291f2c65441591f99fcbb03a37b23857f10f33
e2thenegpii <theallredman@protonmail.com>
2022-12-01 20:41:07 -0800

    Fix wheelfile to work on windows

--- wheelfile.py
+++ wheelfile.py
@@ -38,9 +38,12 @@ import os
 import csv
 import io
 import sys
+import stat
+import time
 import hashlib
 import base64
 import warnings
+import posixpath
 
 from string import ascii_letters, digits
 from pathlib import Path
@@ -102,6 +105,51 @@ def _slots_from_params(func):
     slots.remove('self')
     return slots
 
+class _ZipInfo(zipfile.ZipInfo):
+    """Contains a patched from_file so the function will create valid wheels on windows.
+
+    On windows the buggy version of this function will create an arcname that has windows
+    path seperators.
+
+    A bug report for python will be made shortly.
+    """
+    @classmethod
+    def from_file(cls, filename, arcname=None, *, strict_timestamps=True):
+        """Construct an appropriate ZipInfo for a file on the filesystem.
+
+        filename should be the path to a file or directory on the filesystem.
+
+        arcname is the name which it will have within the archive (by default,
+        this will be the same as filename, but without a drive letter and with
+        leading path separators removed).
+        """
+        if isinstance(filename, os.PathLike):
+            filename = os.fspath(filename)
+        st = os.stat(filename)
+        isdir = stat.S_ISDIR(st.st_mode)
+        mtime = time.localtime(st.st_mtime)
+        date_time = mtime[0:6]
+        if not strict_timestamps and date_time[0] < 1980:
+            date_time = (1980, 1, 1, 0, 0, 0)
+        elif not strict_timestamps and date_time[0] > 2107:
+            date_time = (2107, 12, 31, 23, 59, 59)
+        # Create ZipInfo instance to store file information
+        if arcname is None:
+            arcname = filename
+        arcname = Path(os.path.normpath(os.path.splitdrive(arcname)[1])).as_posix()
+        while arcname[0] in (posixpath.sep, posixpath.altsep):
+            arcname = arcname[1:]
+        if isdir:
+            arcname += '/'
+        zinfo = cls(arcname, date_time)
+        zinfo.external_attr = (st.st_mode & 0xFFFF) << 16  # Unix attributes
+        if isdir:
+            zinfo.file_size = 0
+            zinfo.external_attr |= 0x10  # MS-DOS directory flag
+        else:
+            zinfo.file_size = st.st_size
+
+        return zinfo
 
 # TODO: accept packaging.requirements.Requirement in requires_dist, fix this in
 # example, ensure such objects are converted on __str__
@@ -1938,7 +1986,7 @@ class WheelFile:
 
     def _write_to_zip(self, filename, arcname, skipdir, compress_type,
                       compresslevel):
-        zinfo = zipfile.ZipInfo.from_file(
+        zinfo = _ZipInfo.from_file(
             filename, arcname, strict_timestamps=self._strict_timestamps
         )
 
